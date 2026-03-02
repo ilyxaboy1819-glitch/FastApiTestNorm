66776cf484b5dcb1e1e5681f5be1f5c0310df373
ilya <ilyxaboy1819@gmail.com>
2026-03-02 20:44:53 +0300

    clean master branch (empty branch as required)

--- README.md
+++
@@ -1,3 +0,0 @@
-# FastApiTestNorm
-Async FastAPI CRUD service with database relations
-Change from develop branch
--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import UserModel
-from .application import ApplicationModel
-from .profile import ProfileModel
-from .role import RoleModel
-from .category import CategoryModel
-from .comment import CommentModel
